29d4c0b741ca407693f574d760ab4c2f61a2e851
kwasiesoko <kwasi@esoko.com>
2025-08-17 15:13:11 +0000

    second commit

--- index.js
+++ index.js
@@ -2,51 +2,53 @@ const voiceQueue = require('./queue');
 const Bottleneck = require('bottleneck');
 const axios = require('axios');
 const config = require('./config');
-const accountSid = process.env.TWILIO_ACCOUNT_SID ;
-const authToken =  process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
 
 console.log('👷 Worker started and waiting for jobs...');
-
 const limiter = new Bottleneck({
-    maxConcurrent: 100,
-    minTime: 1000,
+  maxConcurrent: 100,
+  minTime: 1000,
 });
-voiceQueue.process('process',2, async (job) => {
+voiceQueue.process('process', 2, async (job) => {
   console.log('📥 Received job:', job.id);
   console.log('📦 Data:', job.data);
-  let{phoneNumber,id,audioFileUrl}=job.data
-    const voiceUrl = `${config.voicedomainUrl}/voice?filename=${audioFileUrl}`;
-    try {
-      client.calls
-  .create({
-    url:  voiceUrl,
-    to: `+${phoneNumber}`,
-    from: '+233242426295',
-    statusCallback:`${config.voicedomainUrl}/voice/call-status?callSid=${id}` ,
-    statusCallbackMethod: 'POST',
-    tatusCallbackEvent: ['completed'],
-   })
-  .then(call => console.log(call.sid));
-//     const voiceUrl = `https://f27e-154-161-17-232.ngrok-free.app/voice?filename=${encodeURIComponent(audiFileUrl)}`;
-//     const response = await limiter.schedule(async () =>
-//   await client.calls.create({
-//   url: voiceUrl||'https://yourdomain.com/voice',
-//   to: `+${phoneNumber}`,
-//   from: '+233242426295'||'+0987654321',
-//   statusCallback: `https://f27e-154-161-17-232.ngrok-free.app/voice/call-status?callSid=${id}`,  // Twilio sends updates here
-//   statusCallbackMethod: 'POST',
-//   statusCallbackEvent: ['completed'], // or ['initiated', 'ringing', 'completed']
-// }));
-    console.log(`✅ Job ${job.id} processed. Status: `);
-  } catch (err) {
-    console.error(`❌ Failed to process job ${job.id}:`, err.message);
-    throw err; // Bull will retry if configured to
-  }
-  console.log('✅ Done processing job:', job.id);
+  let { phoneNumber, id, audioFileUrl } = job.data;
+
+  executeVoice(phoneNumber, id, audioFileUrl)
+  console.log("ppppppppppppppppppppppppppppppppppppppppppppppppp")
+  console.log(phoneNumber, id, audioFileUrl);
 });
 
 
+
+async function executeVoice(phoneNumber, id, audioFileUrl) {
+  try {
+    const endpoint = `${config.infobipBaseUrl}`
+    const data = {
+      audioFileUrl: audioFileUrl,
+      to: phoneNumber,
+      from: config.infobipPhoneFRom,
+      callbackData: "aseye" + id,
+      notifyUrl: "https://megavoice.elicitlabs.link/infobip/voice-status",
+      notifyContentType: "application/json",
+    };
+    const response = await axios.post(endpoint, payload,{
+      headers: {
+        'Authorization': `App ${config.infobipApiKey}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
+    console.log('Success:', JSON.stringify(response.data));
+  } catch (error) {
+    if (error.response) {
+      console.error('Error response:', error.response.data);
+    } else {
+      console.error('Error message:', error.message);
+    }
+  }
+}
+
+/*
 
 /*
 
--- queue.js
+++ queue.js
@@ -3,9 +3,9 @@ const Queue = require('bull');
 const config = require('./config');
 const voiceQueue = new Queue('voice-message', {
   redis: {
-    host:  config.redisHost||    process.env.REDIS_HOST,
-    port:   config.redisPort ||'6379' || process.env.REDIS_PORT,
-    password: config.redisPassword|| process.env.REDIS_PASSWORD,
+    host:  config.redisHost,
+    port:   config.redisPort ||'6379' ,
+    password: config.redisPassword
   },
 });
 
